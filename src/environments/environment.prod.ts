b924c6642e34a9eb8b8ac531f10ee0133eae935b
Gabriel Jardim <gabrielrlj99@outlook.com>
2022-01-31 11:00:16 -0300

    prep for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURl: "http://localhost:8080/",
+  clientId: "my-angular-app",
+  clientSecret: "@321",
+  token_url: "oauth/token"
 };
